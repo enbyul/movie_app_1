725922d23c705d0d457c6bcfcdcf273ab564da3e
enbyul <50614194+enbyul@users.noreply.github.com>
2021-09-30 23:17:44 +0900

    4강끝

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import PropTypes from "prop-types";
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
+
 
 class App extends React.Component{
   constructor(props){
     super(props);
     console.log("hello");
-    //제일 처음 랜더링 될때 실행 1 (랜더링 시작 시)
+    //제일 처음 랜더링 될때 실행 1 (랜더링 시작 시 - 최초한번실행)
   }
   state = {
     isLoading : true,
     movies : []
   }
-  getMovies = async () => {
-    const {data: {data : {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");//4-1 6:22
-    this.setState({movies, isLoading:false});
+  getMovies = async() => {
+    const {data : {data : {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+                                                                                              //?sort_by=rating rating순으로 정렬
+    this.setState({movies , isLoading : false})
   }
+  
   componentDidMount(){
     console.log("componenr redndsdds")
     //처음 랜더링 됐을때 실행 3 (처음랜더 완료 됐을 때만, 실행되고 그 이후 업데이트 시 실행 x , 새로고침해서 다시 랜더링 되면 초기에 한번만 실행)
@@ -23,16 +28,26 @@ class App extends React.Component{
   }
   componentDidUpdate(){
     console.log("update")
-    //컴포넌트 업데이트 완료되면 실행 3
+    //컴포넌트 업데이트 완료되면 실행 3 (최초 랜더링떄 실행 x)
     //이후 컴포넌트 업데이트시 랜더링2 -> 업데이트3 만 순서대로 실행됨
   }
   render(){
     console.log("render");
     //컴포넌트 먼저 실행 2
-      const { isLoading } = this.state;
-    return <div>
-      {isLoading ? "Loading..." : "we are ready"}
-      </div>
+      const { isLoading, movies } = this.state;
+    return <section className="container">
+      {
+        isLoading ? ( <div className="loadaer"><span className="loader_text">Loading...</span></div>
+        ) : ( 
+          <div className="movies">
+            {movies.map(movie => {
+              console.log(movie.genres);
+              return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
+            })}
+          </div>
+        )
+      }
+      </section>
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie(){
-    return(
-        <h2></h2>
+//state가 필요없을 경우 class컴포넌트 필요없고 function으로 작성
+
+function Movies({ id, year, title, summary, poster, genres}){
+    return (
+        <div className="movies_movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => {
+                        return <li className="genres_genre" key={index}>{genre}</li>
+                    })}
+                </ul>
+                <p className="movie_summary">{summary}</p>
+            </div>
+        </div>
     )
 }
 
-Movie.propTypes = {
-    id : PropTypes.number.isRequired,
-    title : PropTypes.string.isRequired,
-    summary : PropTypes.string.isRequired,
-    poster : PropTypes.string.isRequired,
+Movies.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
+
+export default Movies;
